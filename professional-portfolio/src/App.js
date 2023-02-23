ed0a4ca852edd7dd7fc76d04a1e11c2e90b74460
Andrew Aramouni <aramouni8@gmail.com>
2023-02-23 10:29:45 -0500

    added some css grid

--- professional-portfolio/src/App.js
+++ professional-portfolio/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="header">HEADER</div>
+        <div className="menu">MENU</div>
+        <div className="content">CONTENT</div>
+        <div className="footer">FOOTER</div>
+      </div>
+    </>
   );
 }
 
