fe761d259ceb1ecf07db474b8bf544b0aa9acdae
Andrew Aramouni <aramouni8@gmail.com>
2023-03-29 18:12:32 -0400

    navbar is styled

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,22 +5,30 @@ export default function NavBar() {
     <>
       <div>
         <nav className="navContainer">
-          <NavLink to="/" className="homeLink navItem">
-          <button class="button" type="button" data-hover="HOME" data-active="I'M ACTIVE">
-            <span class="fa fa-home"></span>
-          </button>
+          <NavLink to="/" className="homeLink">
+            <button className="button navItem" type="button" data-hover="Home" data-active="I'M ACTIVE">
+              <span className="fa fa-home"></span>
+            </button>
           </NavLink>
-          <NavLink to="/about" className="aboutLink navItem">
-            <div className="fa-solid fa-person-skiing"></div>
+          <NavLink to="/about" className="aboutLink">
+            <button className="button navItem" type="button" data-hover="About" data-active="I'M ACTIVE">
+              <span className="fa-solid fa-person-skiing"></span>
+            </button>
           </NavLink>
-          <NavLink to="/projects" className="projectsLink navItem">
-            <div className="fa-solid fa-code-branch"></div>
+          <NavLink to="/skills" className="skillsLink">
+            <button className="button navItem" type="button" data-hover="Skills" data-active="I'M ACTIVE">
+              <span className="fa-solid fa-user-ninja"></span>
+            </button>
           </NavLink>
-          <NavLink to="/skills" className="skillsLink navItem">
-            <div className="fa-solid fa-user-ninja"></div>
+          <NavLink to="/projects" className="projectsLink">
+            <button className="button navItem" type="button" data-hover="Projects" data-active="I'M ACTIVE">
+              <span className="fa-solid fa-code-branch"></span>
+            </button>
           </NavLink>
-            <NavLink to="/contact" className="contactLink navItem">
-          <div className="fa-solid fa-address-card"></div>
+          <NavLink to="/contact" className="contactLink">
+            <button className="button navItem" type="button" data-hover="Contact" data-active="I'M ACTIVE">
+              <span className="fa-solid fa-address-card"></span>
+            </button>
         </NavLink>
         </nav>
       </div>
